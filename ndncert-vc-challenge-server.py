b3c618f9310335a759ce2e91820de67895f22c9b
David Barkemeyer <david.barkemeyer@gmx.de>
2022-07-27 09:17:35 -0700

    Replaced arguments 'cred_def_id' and 'requested_attributes' with 'presentation_request'

--- ndncert-vc-challenge-server.py
+++ ndncert-vc-challenge-server.py
@@ -17,23 +17,12 @@ def format_requested_attribute(attr, index, cred_def_id):
         }
     )
 
-def presentation_proof_send_request_body(connection_id, cred_def_id, requested_attributes):
-    requested_attributes = [format_requested_attribute(x, i, cred_def_id) for i, x in enumerate(requested_attributes)]
-
-    presentation_request = {
-        "indy": {
-            "name": "NDNCERT Proof",
-            "requested_attributes": dict(requested_attributes),
-            "requested_predicates": {},
-            "version": "1.0"
-        }
-    }
-
+def presentation_proof_send_request_body(connection_id, presentation_request):
     return {
         "auto_verify": True,
         "comment": "string",
         "connection_id": connection_id,
-        "presentation_request": presentation_request,
+        "presentation_request": json.loads(presentation_request),
         "trace": False
     }
     
@@ -41,8 +30,7 @@ def presentation_proof_send_request_body(connection_id, cred_def_id, requested_a
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--connection_did", required=True)
-    parser.add_argument("--cred_def_id", required=True)
-    parser.add_argument("--requested_attributes", nargs="*", required=True)
+    parser.add_argument("--presentation_request", required=True)
     parser.add_argument("--endpoint", required=True)
     args = parser.parse_args()
     
@@ -52,7 +40,7 @@ async def main():
             text_json = json.loads(text)
             connection_id = text_json["results"][0]["connection_id"]
         
-        ppsr_body = presentation_proof_send_request_body(connection_id, args.cred_def_id, args.requested_attributes)
+        ppsr_body = presentation_proof_send_request_body(connection_id, args.presentation_request)
         async with session.request("post", "/present-proof-2.0/send-request", json=ppsr_body) as resp:
             text = await resp.text()
             text_json = json.loads(text)
