dcca078416f4245e61ba1a79f55df5c02e726ead
David Barkemeyer <david.barkemeyer@gmx.de>
2022-08-15 08:35:07 -0700

    Added Docstrings to Python scripts

--- ndncert-vc-challenge-send-presentation.py
+++ ndncert-vc-challenge-send-presentation.py
@@ -4,25 +4,29 @@ import asyncio
 import json
 import logging
 import sys
+from typing import Dict
 
 from aiohttp import ClientSession, ClientResponse
 
-def send_msg(context, value):
+def send_msg(context: str, value: str) -> None:
+    """ Print message using a simple protocol: <msg>:{context}:{value}. Message is captured in parent process.
+
+    Args:
+        context (str): Message context
+        value (str): Message value
+    """
     print(f"<msg>:{context}:{value}", flush=True)
 
-def format_requested_attribute(attr, index, cred_def_id):
-    attr_label = "attr" + str(index)
-    return (
-        attr_label,
-        {
-            "name": attr,
-            "restrictions": [{
-                "cred_def_id": cred_def_id
-            }]
-        }
-    )
+def presentation_proof_send_request_body(connection_id: str, presentation_request: Dict) -> Dict:
+    """ Build request body for /present-proof-2.0/send-request
 
-def presentation_proof_send_request_body(connection_id, presentation_request):
+    Args:
+        connection_id (str): Connection identifier
+        presentation_request (Dict): Presentation request object
+
+    Returns:
+        Dict: Request body for /present-proof-2.0/send-request
+    """
     return {
         "auto_verify": True,
         "comment": "string",
@@ -34,6 +38,23 @@ def presentation_proof_send_request_body(connection_id, presentation_request):
 async def request(
         session, method, path, data=None, text=False, params=None, headers=None
     ) -> ClientResponse:
+        """ Send a HTTP request
+
+        Args:
+            method (str): HTTP Verb
+            path (str): URL path
+            data (Dict, optional): Request body. Defaults to None.
+            text (bool, optional): If True return response as str, otherwise return response as Dict. Defaults to False.
+            params (Dict, optional): URL parameters. Defaults to None.
+            headers (Dict, optional): Request headers. Defaults to None.
+
+        Raises:
+            Exception: HTTP request failed
+            Exception: Response can not be decoded to JSON
+
+        Returns:
+            ClientResponse: str or Dict representing the response; depends on text argument
+        """
         params = {k: v for (k, v) in (params or {}).items() if v is not None}
         async with session.request(
             method, path, json=data, params=params, headers=headers
@@ -55,6 +76,7 @@ async def request(
     
 
 async def main():
+    # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--connection_did", required=True)
     parser.add_argument("--config_file", required=True)
@@ -62,6 +84,7 @@ async def main():
     args = parser.parse_args()
     logging.basicConfig(level=getattr(logging, args.log), encoding="UTF-8", stream=sys.stdout)
     
+    # Read config file
     with open(args.config_file) as f:
         config = json.load(f)
         logging.debug(f"config: {config}")
@@ -71,10 +94,12 @@ async def main():
     logging.info(f"presentation_request: {presentation_request}")
     
     async with ClientSession() as session:
+        # Retrieve connection to client agent
         res = await request(session, "get", endpoint + "/connections", params={"their_did": args.connection_did})
         connection_id = res["results"][0]["connection_id"]
         logging.info(f"connection_id: {connection_id}")
         
+        # Send presentation request to client agent
         ppsr_body = presentation_proof_send_request_body(connection_id, presentation_request)
         res = await request(session, "post", endpoint + "/present-proof-2.0/send-request", data=ppsr_body)
         thread_id = res["thread_id"]
--- ndncert-vc-challenge-verify-presentation.py
+++ ndncert-vc-challenge-verify-presentation.py
@@ -7,12 +7,35 @@ import sys
 
 from aiohttp import ClientSession, ClientResponse
 
-def send_msg(context, value):
+def send_msg(context: str, value: str) -> None:
+    """ Print message using a simple protocol: <msg>:{context}:{value}. Message is captured in parent process.
+
+    Args:
+        context (str): Message context
+        value (str): Message value
+    """
     print(f"<msg>:{context}:{value}", flush=True)
     
 async def request(
         session, method, path, data=None, text=False, params=None, headers=None
     ) -> ClientResponse:
+        """ Send a HTTP request
+
+        Args:
+            method (str): HTTP Verb
+            path (str): URL path
+            data (Dict, optional): Request body. Defaults to None.
+            text (bool, optional): If True return response as str, otherwise return response as Dict. Defaults to False.
+            params (Dict, optional): URL parameters. Defaults to None.
+            headers (Dict, optional): Request headers. Defaults to None.
+
+        Raises:
+            Exception: HTTP request failed
+            Exception: Response can not be decoded to JSON
+
+        Returns:
+            ClientResponse: str or Dict representing the response; depends on text argument
+        """
         params = {k: v for (k, v) in (params or {}).items() if v is not None}
         async with session.request(
             method, path, json=data, params=params, headers=headers
@@ -34,6 +57,7 @@ async def request(
     
 
 async def main():
+    # Parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--presentation_id", required=True)
     parser.add_argument("--config_file", required=True)
@@ -41,6 +65,7 @@ async def main():
     args = parser.parse_args()
     logging.basicConfig(level=getattr(logging, args.log), encoding="UTF-8", stream=sys.stdout)
     
+    # Read config file
     with open(args.config_file) as f:
         config = json.load(f)
         logging.debug(f"config: {config}")
@@ -48,6 +73,7 @@ async def main():
     logging.info(f"endpoint: {endpoint}")
     
     async with ClientSession() as session:
+        # Retrieve presentation exchange record with client agent
         res = await request(session, "get", endpoint + "/present-proof-2.0/records", params={"thread_id": args.presentation_id})
         res = res["results"][0]
         pres_ex_id = res["pres_ex_id"]
